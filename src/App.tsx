f01e64aa6b62934c50b52cb08510c21890a849b9
Angelina <zhuravskayalina@gmail.com>
2023-02-06 18:03:20 +0300

    docs: update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import './App.scss';
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+import styles from './App.scss';
 
-const App = (): JSX.Element => {
-  return <div className='App'>Hi</div>;
+import { ReactComponent as MenuIcon } from './assets/icons/menu.svg';
+
+const cx = classNames.bind(styles);
+
+const App = () => {
+  const [value, setValue] = useState<boolean>(false);
+  const buttonStateClass = value ? 'rose' : 'blue';
+
+  return (
+    <div className={cx('app')}>
+      <MenuIcon />
+      <p className={cx(buttonStateClass)}>Rose</p>
+
+      <button type='button' onClick={() => setValue((prevState) => !prevState)}>
+        Change Rose to Blue
+      </button>
+      <button type='button' className={cx('icon-voice')} />
+    </div>
+  );
 };
 
 export default App;
