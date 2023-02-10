e49e9f8814e6a94c68315fd1d273d07ba7623f1f
Angelina <zhuravskayalina@gmail.com>
2023-02-10 18:17:39 +0300

    refactor: rename components and directories, move variables and mixins to appropriate directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './App.scss';
 
-import MainContainer from './components/mainContainer/MainContainer';
-import ProfileSection from './components/profileSection/ProfileSection';
-import Header from './components/header/header';
+import MainContainer from './components/MainContainer/MainContainer';
+import ProfileSection from './components/ProfileSection/ProfileSection';
+import Header from './components/Header/Header';
 
 import profileImg from './assets/images/max.jpeg';
 
