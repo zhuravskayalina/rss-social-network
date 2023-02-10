2072b69eda41dbcdc80efa002d50a3af0e7ca495
Angelina <zhuravskayalina@gmail.com>
2023-02-11 00:33:02 +0300

    refactor: simplify className dynamic adding

--- src/components/ProfileSection/Navigation/NavItem/NavItem.tsx
+++ src/components/ProfileSection/Navigation/NavItem/NavItem.tsx
@@ -7,13 +7,14 @@ const cx = classNames.bind(styles);
 
 const NavItem = ({ title, url }: NavItemProps) => {
   const basicClassName = 'nav-item__link';
+  const activeClassName = 'nav-item__link_active';
 
   return (
     <li className={cx('nav-item')}>
       <NavLink
         to={url}
         end
-        className={({ isActive }) => cx(basicClassName, { 'nav-item__link_active': isActive })}
+        className={({ isActive }) => cx(basicClassName, { [activeClassName]: isActive })}
       >
         {title}
       </NavLink>
