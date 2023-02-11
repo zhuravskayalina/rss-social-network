09cc9b1d699a4c93c1d51c3533de6f29eec36e3c
Pavel <tr.pav.ar@gmail.com>
2023-02-11 22:15:04 +0300

    feat: add adaptability

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,30 +1,29 @@
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import styles from './mainPage.module.scss';
-import Header from '../header/header';
-import DiscoverButton from './discoverButton/dicoverButton';
+import mainImg from '../../assets/images/main_img.png';
+import DiscoverButton from './discoverButton/DicoverButton';
 
 const cn = classNames.bind(styles);
 
 const MainPage = () => {
   return (
-    <div>
-      <Header />
-      <div className={cn('main')}>
-        <div className={cn('main__content')}>
-          <h1 className={cn('main__content-title')}>OWL Community</h1>
-          <p className={cn('main__content-text')}>
-            Having real social contacts can sometimes be difficult FUN, everything becomes much
-            simpler!
-          </p>
-          <p className={cn('main__content-count')}>10,95,219</p>
-          <p>Connected People</p>
-          <Link to='/'>
-            <DiscoverButton />
-          </Link>
-        </div>
+    <div className={cn('main')}>
+      <div className={cn('main__content')}>
+        <h1 className={cn('main__content-title')}>OWL Community</h1>
+        <p className={cn('main__content-text')}>
+          Having real social contacts can sometimes be difficult FUN, everything becomes much
+          simpler!
+        </p>
+        <p className={cn('main__content-count')}>10,95,219</p>
+        <p>Connected People</p>
+        <Link to='/ '>
+          <DiscoverButton />
+        </Link>
       </div>
-      <div>Footer</div>
+      <div className={cn('main__img')}>
+        <img src={mainImg} alt='People' />
+      </div>
     </div>
   );
 };
