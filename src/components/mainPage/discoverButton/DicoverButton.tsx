b2b76c1f21eb5dc0dacbb12bb83aa65257da9d70
Pavel <tr.pav.ar@gmail.com>
2023-02-12 23:15:09 +0300

    fix: rename variables, changed method of using images, add interactivity to elements

--- src/components/mainPage/discoverButton/DicoverButton.tsx
+++ src/components/mainPage/discoverButton/DicoverButton.tsx
@@ -1,16 +1,15 @@
 import classNames from 'classnames/bind';
-
 import styles from './discoverButton.module.scss';
-import arrow from '../../../assets/icons/arrow.svg';
+import { ReactComponent as Arrow } from '../../../assets/icons/arrow.svg';
 
-const cn = classNames.bind(styles);
+const cx = classNames.bind(styles);
 
 const DiscoverButton = () => {
   return (
-    <div className={cn('discoverButton')}>
-      <div className={cn('discoverButton__text')}>Discover Now</div>
-      <div className={cn('discoverButton__arrow')}>
-        <img src={arrow} alt='Arrow' />
+    <div role='button' className={cx('discoverButton')}>
+      <div className={cx('discoverButton__text')}>Discover Now</div>
+      <div className={cx('discoverButton__arrow')}>
+        <Arrow />
       </div>
     </div>
   );
