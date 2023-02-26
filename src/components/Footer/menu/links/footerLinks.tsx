22be7f0e0e66d8da17cc95255ddb7b0bef420d64
esmolina <elenasmolinann@gmail.com>
2023-02-26 16:26:22 +0300

    fix: resolve error in footer keys

--- src/components/Footer/menu/links/footerLinks.tsx
+++ src/components/Footer/menu/links/footerLinks.tsx
@@ -4,14 +4,17 @@ export const importantLinks = [
   {
     title: <FormattedMessage id='aboutAs' />,
     link: '/profile/1',
+    id: 1,
   },
   {
     title: <FormattedMessage id='contactUs' />,
     link: '#!',
+    id: 2,
   },
   {
     title: <FormattedMessage id='help' />,
     link: '#!',
+    id: 3,
   },
 ];
 
@@ -19,18 +22,22 @@ export const communityLinks = [
   {
     title: <FormattedMessage id='newsfeed' />,
     link: '/newsfeed',
+    id: 4,
   },
   {
     title: <FormattedMessage id='groups' />,
     link: '#!',
+    id: 5,
   },
   {
     title: <FormattedMessage id='friends' />,
     link: '/profile/friends',
+    id: 6,
   },
   {
     title: <FormattedMessage id='forums' />,
     link: '#!',
+    id: 7,
   },
 ];
 
@@ -38,13 +45,16 @@ export const developersLinks = [
   {
     title: <FormattedMessage id='anhelina' />,
     link: 'https://github.com/zhuravskayalina',
+    id: 8,
   },
   {
     title: <FormattedMessage id='lena' />,
     link: 'https://github.com/esmolina',
+    id: 9,
   },
   {
     title: <FormattedMessage id='pasha' />,
     link: 'https://github.com/pavel-troinich',
+    id: 10,
   },
 ];
