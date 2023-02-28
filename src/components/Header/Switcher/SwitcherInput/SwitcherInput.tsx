817f6c2f531cdd0a1485e0f1fec0bb120184860f
esmolina <elenasmolinann@gmail.com>
2023-02-28 15:20:22 +0300

    feat: add hot key for send message

--- src/components/Header/Switcher/SwitcherInput/SwitcherInput.tsx
+++ src/components/Header/Switcher/SwitcherInput/SwitcherInput.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from './switcher.module.scss';
 import { LOCALES } from '../../../../IntlLocale/locales';
@@ -8,14 +9,21 @@ const cx = classNames.bind(styles);
 const SwitcherInput = ({ currentLocale, handleChange }: SwitcherInputProps) => {
   const isRussian = currentLocale === LOCALES.RUSSIAN;
 
+  // useEffect(() =>
+  //   document.addEventListener('keydown', () => {
+  //   }),
+  // );
+
   return (
     <label htmlFor='language-switcher' className={cx('header__switch-language')}>
       <input
         type='checkbox'
+        tabIndex={0}
         id='language-switcher'
         className={cx('header__switch-language-input')}
         onChange={handleChange}
         defaultChecked={isRussian}
+        checked={isRussian}
       />
       <span className={cx('header__switch-language-circle')} />
     </label>
